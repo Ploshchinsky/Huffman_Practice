95f1a28d0fb00b218d046b4ec09c1ba6e53e1770
Anton P. <made1of1fire@gmail.com>
2024-06-17 15:38:22 +0500

    fix(main): update project structure

---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,57 @@
+package org.example;
+
+public class Node implements Comparable<Node> {
+    private Character data;
+    private int weight;
+    private Node left;
+    private Node right;
+
+    public Node(Character data, int weight) {
+        this.data = data;
+        this.weight = weight;
+    }
+
+    public Node(Character data, int weight, Node left, Node right) {
+        this.data = data;
+        this.weight = weight;
+        this.left = left;
+        this.right = right;
+    }
+
+    public Character getData() {
+        return data;
+    }
+
+    public void setData(Character data) {
+        this.data = data;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return o.weight - this.weight;
+    }
+}
