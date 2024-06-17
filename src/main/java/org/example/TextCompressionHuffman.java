8802e11e1100a48c5d8e38b2b49960b878f88e5a
Anton P. <made1of1fire@gmail.com>
2024-06-17 16:12:19 +0500

    feat(TextCompressionHuffman.class): added method getNodesFromMap() -> return List on Nodes from character frequency map

--- src/main/java/org/example/TextCompressionHuffman.java
+++ src/main/java/org/example/TextCompressionHuffman.java
@@ -1,15 +1,21 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class TextCompressionHuffman {
     public static Map<Character, Integer> getCharFreq(String text) {
         Map<Character, Integer> charFreq = new HashMap<>();
         for (int i = 0; i < text.length(); i++) {
-            charFreq.put(text.charAt(i), charFreq.getOrDefault(text.charAt(i), 1) + 1);
+            charFreq.put(text.charAt(i), charFreq.getOrDefault(text.charAt(i), 0) + 1);
         }
         return charFreq;
+    }
+
+    public static List<Node> getNodesFromMap(Map<Character, Integer> charFreqMap) {
+        List<Node> nodeList = new ArrayList<>();
+        for (Map.Entry<Character, Integer> entry : charFreqMap.entrySet()) {
+            nodeList.add(new Node(entry.getKey(), entry.getValue()));
+        }
+        return nodeList;
     }
 }
