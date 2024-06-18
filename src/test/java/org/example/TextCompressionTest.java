0b25c0789d22760c429238656d51ac6856122cd8
Anton P. <made1of1fire@gmail.com>
2024-06-18 14:54:43 +0500

    feat(test): added TextcompressionTest.class -> added unit tests to check the work of all TextCompressionHuffman.class metodes

---
+++ src/test/java/org/example/TextCompressionTest.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+public class TextCompressionTest {
+    static String text = "Nested classes are classes defined inside other classes. " +
+            "They can be static (static nested classes) or non-static (inner classes)";
+    static String text2 = "aa ba aab caa";
+
+    @Test
+    public void TextCompressionHuffmanTest() {
+        Map<Character, Integer> charFreqMap = TextCompressionHuffman.getCharFreq(text2);
+        for (Map.Entry<Character, Integer> entry : charFreqMap.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+        System.out.println("----");
+
+        List<BinaryTreeNode> nodeList = TextCompressionHuffman.getNodesFromMap(charFreqMap);
+        Collections.sort(nodeList);
+        System.out.println(nodeList);
+        System.out.println("----");
+
+        BinaryTreeNode tree = TextCompressionHuffman.getTreeFromNodes(nodeList);
+        Map<Character, String> codes = new HashMap<>();
+        for (Character c : charFreqMap.keySet()) {
+            codes.put(c, TextCompressionHuffman.huffmanSerialized(c, new String(), tree));
+        }
+        System.out.println(codes);
+        System.out.println("----");
+
+        String encoded = "";
+        for (int i = 0; i < text2.length(); i++) {
+            encoded += codes.get(text2.charAt(i));
+        }
+        System.out.println(encoded);
+        System.out.println("----");
+        System.out.println(TextCompressionHuffman.huffmanDeserialized(encoded, tree));
+    }
+
+    @Test
+    public void Test() {
+        String encodeString = TextCompressionHuffman.encode(text2);
+        String decodeString = TextCompressionHuffman.decode(encodeString);
+        System.out.println(encodeString);
+        System.out.println(decodeString);
+        System.out.println("----");
+        encodeString = TextCompressionHuffman.encode(text);
+        decodeString = TextCompressionHuffman.decode(encodeString);
+        System.out.println(encodeString);
+        System.out.println(decodeString);
+        System.out.println("----");
+    }
+}
